767badac86a30e88598a1cdd7b0cb2b5eae786d9
Lucho <44885834+attrix182@users.noreply.github.com>
2020-11-23 11:59:03 -0300

    Add files via upload

---
+++ user.js
@@ -0,0 +1,80 @@
+const user = {};
+
+var mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'merceriabd'
+});
+
+user.list = (req, res) => {
+
+    const sql = 'SELECT * FROM usuarios'
+
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('no results')
+        }
+
+    })
+
+};
+
+user.listID = (req, res) => {
+
+
+    const {id }= req.params;
+    const sql = `SELECT * FROM usuarios WHERE ID = ${id}`
+
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('no results')
+        }
+
+    })
+
+};
+
+user.add = (req, res) => {
+
+
+    const {id }= req.params;
+    const sql = 'INSERT INTO usuarios WHERE SET ?'
+
+    const newUser = {
+  /*       correo: req.body.name;
+        clave
+        nombre
+        apellido
+        perfil
+        foto
+ */
+    }
+
+
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            res.json(results);
+        } else {
+            res.send('no results')
+        }
+
+    })
+
+};
+
+
+
+
+
+
+module.exports = user;
